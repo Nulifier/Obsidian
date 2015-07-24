fac89b4c71ee8cd5875a9e48c50aad132b70ae56
Jeffrey Steward <nulifier@gmail.com>
2015-07-24 11:31:30 -0600

    Added test coverage to formatError.js

---
+++ test/helpers.formatError.js
@@ -0,0 +1,31 @@
+"use script";
+
+var formatError		= require("../lib/helpers/formatError");
+
+describe("formatError", function() {
+	it("should be a function", function() {
+		formatError.should.be.a("function");
+	});
+
+	it("should respond with a string", function() {
+		var ret = formatError("An Error");
+		ret.should.be.a("string");
+	});
+
+	it("should include the title", function() {
+		var title = "Totally random string";
+		var ret = formatError(title);
+		ret.should.contain(title);
+	});
+
+	it("should include the message", function() {
+		var msg = "This is the descriptive error message";
+		var ret = formatError("An Error", msg);
+		ret.should.contain(msg);
+	});
+
+	it("should have a blank msg when not provided", function() {
+		var ret = formatError("An Error");
+		ret.should.contain("<pre></pre>");
+	});
+});
