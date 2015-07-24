f4fe67b00dce1b6c912f508c9debf9783b6c12c3
Jeffrey Steward <nulifier@gmail.com>
2015-07-24 13:51:23 -0600

    Added some new tests.

---
+++ test/core.middleware.compression.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var compression	= require("../lib/core/middleware/1-compression");
+
+describe("core.middleware.compression", function() {
+	var fakeThis;
+	var compressEnabled
+
+	beforeEach("create fake this", function() {
+		fakeThis = {
+			enabled: sinon.stub().withArgs("compress").returns(true),
+			app: {
+				use: sinon.spy()
+			}
+		}
+	});
+
+	it("should be a function", function() {
+		compression.should.be.a("function");
+	});
+
+	it("should only check the compress option", function() {
+		compression.call(fakeThis);
+		fakeThis.enabled.should.be.calledOnce;
+		fakeThis.enabled.should.be.calledWith("compress");
+	});
+
+	it("should use one middleware function if enabled", function() {
+		compression.call(fakeThis);
+		fakeThis.app.use.should.be.calledOnce;
+		// express-compress returns an array of middleware
+		fakeThis.app.use.args[0][0].should.be.a("function");
+	});
+
+	it("should not use any middleware if not enabled", function() {
+		fakeThis.enabled = sinon.stub().withArgs("compress").returns(false);
+		compression.call(fakeThis);
+		fakeThis.app.use.should.not.be.called;
+	});
+});
