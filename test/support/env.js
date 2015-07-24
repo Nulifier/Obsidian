6a21dc250077dee662dc9154b2daf0eb98199ee8
Jeffrey Steward <nulifier@gmail.com>
2015-07-24 13:51:06 -0600

    Added sinon.

--- test/support/env.js
+++ test/support/env.js
@@ -5,4 +5,14 @@
 process.env.NODE_ENV = "test";
 
 var chai		= require("chai");
+var sinon		= require("sinon");
+var sinonChai	= require("sinon-chai");
+
+// Install should assertions
 global.should	= chai.should();
+
+// Install sinon
+global.sinon	= sinon;
+
+// Install sinon-chai
+chai.use(sinonChai);
